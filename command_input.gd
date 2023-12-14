bfdc11f7de0c917527c5972eb00c5a453599cc8b
Shuflduf <michaelssimeonoff@gmail.com>
2023-12-14 15:14:10 -0700

    UI

--- command_input.gd
+++ command_input.gd
@@ -1,9 +1,46 @@
 extends Control
 
+var box = preload("res://rigid_body_3d.tscn")
+
 @onready var object_count_label = $CommandCanvas/ObjectCount
+
+@onready var square_mesh = %SquareMesh
+@onready var square_coll = %SquareColl
+@onready var sphere_mesh = %SphereMesh
+@onready var sphere_coll = %SphereColl
+@onready var reset = %Reset
+
+signal square_mesh_button
+signal square_coll_button
+signal sphere_mesh_button
+signal sphere_coll_button
+
+signal reset_button
 
 var object_count := 0
 
 func _on_jolt_new_box_spawn():
 	object_count += 1
 	object_count_label.text = "Objects: " + str(object_count)
+	
+func _ready():
+#region signals
+
+	sphere_mesh.pressed.connect(func():
+		emit_signal("sphere_mesh_button")
+	)
+	square_mesh.pressed.connect(func():
+		emit_signal("square_mesh_button")	
+	)
+	square_coll.pressed.connect(func():
+		emit_signal("square_coll_button")	
+	)
+	sphere_coll.pressed.connect(func():
+		emit_signal("sphere_coll_button")	
+	)
+
+#endregion
+	reset.pressed.connect(func():
+		emit_signal("reset_button")
+		object_count = 0
+	)
--- jolt.gd
+++ jolt.gd
@@ -1,25 +1,74 @@
 extends Node3D
 
-var box = preload("res://rigid_body_3d.tscn")
+var shape := preload("res://shapes/box_box.tscn")
+
+const BOX_BOX = preload("res://shapes/box_box.tscn")
+const BOX_SPHERE = preload("res://shapes/box_sphere.tscn")
+const SPHERE_BOX = preload("res://shapes/sphere_box.tscn")
+const SPHERE_SPHERE = preload("res://shapes/sphere_sphere.tscn")
+
 # Called when the node enters the scene tree for the first time.
 @onready var box_parent = $BoxParent
 @onready var marker = $Marker3D
 @onready var command_input = $CommandInput
 
-@export var min := -100
-@export var max := 100
+@export var spread := 2
+
+signal set_square_mesh
+signal set_square_coll
+signal set_sphere_mesh
+signal set_sphere_coll
 
 signal new_box_spawn
 
 func _physics_process(delta):
-	var new_box = box.instantiate()
+	var new_box = shape.instantiate()
 
-	new_box.position.x = marker.global_position.x + randf_range(max, min)
-	new_box.position.y = marker.global_position.y + randf_range(max, min)
-	new_box.position.z = marker.global_position.z + randf_range(max, min)
-	#new_box.rotation.z = randf_range(180, -180)
+	new_box.position.x = marker.global_position.x + randf_range(spread, -spread)
+	new_box.position.y = marker.global_position.y + randf_range(spread, -spread)
+	new_box.position.z = marker.global_position.z + randf_range(spread, -spread)
+	
+	new_box.rotation.z = randf_range(180, -180)
+	new_box.rotation.x = randf_range(180, -180)
+	new_box.rotation.y = randf_range(180, -180)
+	
 	box_parent.add_child(new_box)
 	emit_signal("new_box_spawn")
 
 func _on_area_3d_body_entered(body):
 	body.queue_free()
+
+#region stuff
+
+func _ready():
+#region shape change
+	command_input.square_coll_button.connect(func():
+		if shape == SPHERE_SPHERE:
+			shape = SPHERE_BOX
+		elif shape == BOX_SPHERE:
+			shape = BOX_BOX
+	)
+	command_input.sphere_coll_button.connect(func():
+		if shape == SPHERE_BOX:
+			shape = SPHERE_SPHERE
+		elif shape == BOX_BOX:
+			shape = BOX_SPHERE
+	)
+	command_input.square_mesh_button.connect(func():
+		if shape == SPHERE_SPHERE:
+			shape = BOX_SPHERE
+		elif shape == SPHERE_BOX:
+			shape = BOX_BOX
+	)
+	command_input.sphere_mesh_button.connect(func():
+		if shape == BOX_SPHERE:
+			shape = SPHERE_SPHERE
+		elif shape == BOX_BOX:
+			shape = SPHERE_BOX
+	)
+
+#endregion
+	command_input.reset_button.connect(func():
+		for child in box_parent.get_children():
+			child.queue_free()
+	)
